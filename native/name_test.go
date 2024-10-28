483edb6d719d784822b12303eb84c17f2338e0d9
丁奎 <59461202@qq.com>
2024-10-28 15:45:51 +0800

    nativeTest修改

--- native/name_test.go
+++ native/name_test.go
@@ -1,12 +1,13 @@
-package native
+package native_test
 
 import (
 	"gitee.com/dk83/goutils/dlog"
+	"gitee.com/dk83/goutils/native"
 	"testing"
 )
 
 func TestFileName(t *testing.T) {
-	set := NewFileNameSet()
+	set := native.NewFileNameSet()
 	dlog.Info(set.Unique_file_name("x/te st.json"))
 	dlog.Info(set.Unique_file_name("t est.json"))
 	dlog.Info(set.Unique_file_name("tes t.json"))
